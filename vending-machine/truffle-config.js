ca2d5416be3d6d3c8703ec30d9167403820bffe3
amatureApe <amatureApe@gmail.com>
2022-10-13 22:27:10 -0500

    more truffle setup

--- vending-machine/truffle-config.js
+++ vending-machine/truffle-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -82,6 +86,18 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
+    goerli: {
+      provider: () => new HDWalletProvider({
+        privateKeys: [process.env.PRIVATE_KEY_1],
+        providerOrUrl: ``,
+        numberOfAddresses: 1
+      }),
+      network_id: 5,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks 200,
+      skipDryRun: true
+    },
     // goerli: {
     //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
     //   network_id: 5,       // Goerli's id
